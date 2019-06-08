e523c7cb4d636ad4405f60277c36fe7b9637f4a0
Ethan Brierley <ethanboxx@gmail.com>
2019-06-08 19:20:09 +0100

    ?

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use rand::Rng;
+use std::collections::HashMap;
+
+const TESTS_TO_DO: u32 = 5000000;
+
 fn main() {
     println!("Hello, world!");
+    let mut map = HashMap::new();
+    for x in 1..=6 {
+        (&mut map).insert(x, 1);
+    }
+    for _ in 0..TESTS_TO_DO {
+        (&mut map)
+            .entry(die_roll())
+            .and_modify(|e| *e += 1)
+            .or_insert(1);
+    }
+    dbg!(map);
+}
+
+fn die_roll() -> u32 {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(1, 7)
 }
